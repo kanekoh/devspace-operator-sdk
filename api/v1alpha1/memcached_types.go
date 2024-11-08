3093751a075ba96ff7f558f93f80b9c7d3798746
Hiroyuki Kaneko <hkaneko@redhat.com>
2024-11-08 01:34:10 +0000

    define api

--- api/v1alpha1/memcached_types.go
+++ api/v1alpha1/memcached_types.go
@@ -30,18 +30,27 @@ type MemcachedSpec struct {
 
 	// Foo is an example field of Memcached. Edit memcached_types.go to remove/update
 	Foo string `json:"foo,omitempty"`
+
+
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Minimum=0
+	Size int32 `json:"size"`
 }
 
 // MemcachedStatus defines the observed state of Memcached
 type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
 // Memcached is the Schema for the memcacheds API
+//+kubebuilder:subresource:status
 type Memcached struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
